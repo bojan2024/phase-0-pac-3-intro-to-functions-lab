52ee01524f3e279e8799aa62057a12e89be5299e
macak <bojanspremo@hotmail.com>
2024-05-09 18:40:51 -0500

    Writes function codes in the index.js

--- index.js
+++ index.js
@@ -14,16 +14,10 @@ function logWhisper(string) {
 //logWhisper("HELLO");
 
 function sayHiToHeadphonedRoommate(string) {
-   //if (string === string.toLowerCase); {
-   // return "I can\'t hear you!";}
-   //if (string === string.toUpperCase); {
-   // return "YES INDEED!";}
-    //if (string === "Let\'s have dinner together!");{
-    //return "I would love to!";}
     if (string === string.toLowerCase()) {     
-        return "I can\'t hear you!";    
+    return "I can\'t hear you!";    
     } if (string === string.toUpperCase()) {
-   return "YES INDEED!";
+    return "YES INDEED!";
     } if (string === "Let\'s have dinner together!") {
     return "I would love to!";
 }}
